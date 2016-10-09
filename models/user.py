9aa63757586974265864745adda8d7ca9f4c809a
ocazuer <ocazuer@gmail.com>
2016-10-09 19:41:58 +0800

    使用 md5 加密密码

--- models/user.py
+++ models/user.py
@@ -4,12 +4,15 @@ from . import timestamp
 
 import hashlib
 
+def hash_md5(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
 
 class User(db.Model, ModelMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True)
-    # 明文储存密码
+    # md5 密文储存密码
     password = db.Column(db.String())
     avatar = db.Column(db.String())
     created_time = db.Column(db.Integer)
@@ -21,12 +24,15 @@ class User(db.Model, ModelMixin):
     def __init__(self, form):
         self.created_time = timestamp()
         self.username = form.get('username', '')
-        self.password = form.get('password', '')
+        self.password = hash_md5(form.get('password', ''))
         self.avatar = self.random_avatar()
 
 
     def validate_register(self):
-        return len(self.username) > 2 and (User.query.filter_by(username=self.username).first() is None)
+        c1 = len(self.username) >= 2
+        c2 = len(self.username) <= 20
+        c3 = User.query.filter_by(username=self.username).first() is None
+        return c1 and c2 and c3
 
     def validate_login(self, u):
         return u.username == self.username and u.password == self.password
